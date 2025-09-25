ca69dc2aaf0eab97ce3c92161ba167e6750729da
IRONDESK <zerozerobase@gmail.com>
2025-09-26 00:51:53 +0900

    - Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,20 @@
-import type { Metadata } from 'next';
-import '@/style/global.css';
-import localFont from 'next/font/local';
-import { AuthProvider } from '@/contexts/AuthContext';
-import Layout from '@/share/layouts/Layout';
-import { Provider as JotaiProvider } from 'jotai';
-import { OverlayProvider } from 'overlay-kit';
+import type { Metadata } from "next";
+import "@/style/global.css";
+import localFont from "next/font/local";
+import { AuthProvider } from "@/contexts/AuthContext";
+import Layout from "@/share/layouts/Layout";
+import { Provider as JotaiProvider } from "jotai";
+import { OverlayProvider } from "overlay-kit";
+import { theme } from "@/style/theme.css";
 
 export const metadata: Metadata = {
-  title: '오떠끼 - 오늘의 떡볶이를 찾아보세요',
-  description: '떡볶이, 분식집을 자세하게 찾아볼 수 있어요',
+  title: "오떠끼 - 오늘의 떡볶이를 찾아보세요",
+  description: "떡볶이, 분식집을 자세하게 찾아볼 수 있어요",
 };
 const pretendard = localFont({
-  src: './fonts/PretendardVariable.woff2',
-  display: 'swap',
-  weight: '45 920',
+  src: "./fonts/PretendardVariable.woff2",
+  display: "swap",
+  weight: "45 920",
 });
 
 export default function RootLayout({
@@ -23,7 +24,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={pretendard.className}>
+      <body
+        className={pretendard.className}
+        style={{ color: theme.color.gray["800"] }}
+      >
         <JotaiProvider>
           <AuthProvider>
             <OverlayProvider>
