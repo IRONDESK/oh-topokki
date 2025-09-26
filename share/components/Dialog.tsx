6e6b51cafdb857b81e0f9a3ec5b03da17ad51c41
SOOCHUL SON <87234410+IRONDESK@users.noreply.github.com>
2025-09-26 17:28:24 +0900

    - dialog 컴포넌트 및 overlay 공통 함수

---
+++ share/components/Dialog.tsx
@@ -0,0 +1,51 @@
+import type { ComponentProps } from 'react';
+import type { OverlayAsyncControllerComponent, OverlayControllerComponent } from 'overlay-kit';
+import { dialogStyle } from '@/share/common/css/dialog.css.ts';
+import { flexs } from '@/style/container.css.ts';
+
+type AlertBaseProps = ComponentProps<OverlayControllerComponent>;
+type ConfirmBaseProps = ComponentProps<OverlayAsyncControllerComponent<boolean>>;
+export type DialogCommon = {
+  title: string;
+  contents?: string;
+};
+
+export type DialogProps =
+  | ({ type: 'alert' } & DialogCommon & AlertBaseProps)
+  | ({
+      type: 'confirm';
+    } & DialogCommon &
+      ConfirmBaseProps);
+
+export const Dialog = (props: DialogProps) => {
+  const { type, isOpen, close, title, contents } = props;
+  const handleClose = (resolve: boolean) => {
+    if (type === 'confirm') {
+      close(resolve);
+    } else {
+      close();
+    }
+  };
+
+  if (!isOpen) return null;
+  return (
+    <section className={dialogStyle.dim} onClick={type === 'confirm' ? undefined : () => handleClose}>
+      <div className={dialogStyle.container}>
+        <div className={dialogStyle.inner}>
+          <p className={dialogStyle.title}>{title}</p>
+          {contents && <p className={dialogStyle.contents}>{contents}</p>}
+        </div>
+        <div className={flexs({ gap: '8' })}>
+          {type === 'confirm' && (
+            <button type='button' onClick={() => close(false)}>
+              취소
+            </button>
+          )}
+          <button type='button' onClick={() => handleClose(true)}>
+            확인
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
