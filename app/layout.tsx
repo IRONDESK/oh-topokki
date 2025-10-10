33fbc05378897c301c527cba6a8ea3ff1000e879
IRONDESK <zerozerobase@gmail.com>
2025-10-11 02:53:17 +0900

    - 레이아웃 viewport 설정 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "@/style/global.css";
 import localFont from "next/font/local";
 import Layout from "@/share/layouts/Layout";
@@ -8,6 +8,17 @@ import { theme } from "@/style/theme.css";
 export const metadata: Metadata = {
   title: "오떠끼 - 오늘의 떡볶이를 찾아보세요",
   description: "떡볶이, 분식집을 자세하게 찾아볼 수 있어요",
+};
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1.0,
+  maximumScale: 1.0,
+  userScalable: false,
+  viewportFit: "contain",
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "#f7fafc" },
+    { media: "(prefers-color-scheme: dark)", color: "#111012" },
+  ],
 };
 const pretendard = localFont({
   src: "./fonts/PretendardVariable.woff2",
