4ec18c9e221922e7a08c6cacbe7eb9b3252781b1
SOOCHUL SON <87234410+IRONDESK@users.noreply.github.com>
2025-10-13 11:54:59 +0900

    - apple-mobile-web-app-status-bar-style 값 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
     <html lang="ko">
        <head>
         <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
       </head>
       <body
         className={pretendard.className}
