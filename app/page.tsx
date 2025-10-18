1af4377cae1a77b54bba599ab07c7ea37ce5241e
IRONDESK <zerozerobase@gmail.com>
2025-10-18 19:53:37 +0900

    - 최조 지도 위치를 현재 위치로 반영

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 'use client';
 
 import TopokkiMap from '@/components/map/TopokkiMap';
+import { useMapLocation } from '@/hooks/useMapLocation';
 
 export default function Home() {
+  const { currentLocation } = useMapLocation();
+
   return (
     <>
-      <TopokkiMap />
+      <TopokkiMap center={currentLocation ?? undefined} />
     </>
   );
 }
