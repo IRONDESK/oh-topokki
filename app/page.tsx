9a02ffe8436eaf01e9e49f76746bbbda90b3e70e
IRONDESK <zerozerobase@gmail.com>
2025-10-25 17:19:15 +0900

    - SearchParams 사용시 Suspense 감싸기

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,30 @@
 "use client";
 
-import TopokkiMap from "@/components/map/TopokkiMap";
+import { Suspense } from "react";
 import { useMapLocation } from "@/hooks/useMapLocation";
 import { useSearchParams } from "next/navigation";
+
+import TopokkiMap from "@/components/map/TopokkiMap";
 import OpenDetailPage from "@/app/OpenDetailPage";
 
-export default function Home() {
-  const { currentLocation } = useMapLocation();
+function HomeContent() {
   const params = useSearchParams();
   const restaurantId = params.get("restaurant");
 
+  if (restaurantId) {
+    return <OpenDetailPage restaurantId={restaurantId} />;
+  }
+  return null;
+}
+
+export default function Home() {
+  const { currentLocation } = useMapLocation();
+
   return (
     <>
-      {restaurantId && <OpenDetailPage restaurantId={restaurantId} />}
+      <Suspense>
+        <HomeContent />
+      </Suspense>
       <TopokkiMap center={currentLocation ?? undefined} />
     </>
   );
