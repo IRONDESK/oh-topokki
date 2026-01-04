1cabf72b1ec63c70e78d8d588457424440e6ffdf
IRONDESK <zerozerobase@gmail.com>
2026-01-04 17:40:28 +0900

    - install sonner

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -4,6 +4,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { OverlayProvider } from "overlay-kit";
 import { Provider as JotaiProvider } from "jotai";
+import { Toaster } from "sonner";
+import { typo } from "@/style/typo.css";
 
 function Providers({ children }: { children: React.ReactNode }) {
   const queryClient = new QueryClient({});
@@ -12,6 +14,24 @@ function Providers({ children }: { children: React.ReactNode }) {
     <QueryClientProvider client={queryClient}>
       <JotaiProvider>
         <AuthProvider>
+          <Toaster
+            duration={2000}
+            position="top-center"
+            className={typo({
+              size: "body3",
+              weight: "medium",
+            })}
+            toastOptions={{
+              style: {
+                backgroundColor: "rgba(0,0,0,0.8)",
+                color: "#fff",
+                borderRadius: "10px",
+                fontSize: "inherit",
+                borderColor: "rgba(0,0,0,0.4)",
+                backdropFilter: "blur(4px)",
+              },
+            }}
+          />
           <OverlayProvider>{children} </OverlayProvider>
         </AuthProvider>
       </JotaiProvider>
